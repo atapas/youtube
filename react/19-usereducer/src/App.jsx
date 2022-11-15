e95bd254b8926c6751cd5fe5d91d6663a30bfcf3
Tapas Adhikary <tapas.adhikary@gmail.com>
2022-11-15 17:10:36 +0530

    Done a basic wish BucketList

--- react/19-usereducer/src/App.jsx
+++ react/19-usereducer/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 
+import BucketList from './BucketList';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <BucketList />
     </div>
   )
 }
