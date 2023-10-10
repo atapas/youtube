b1a133b04745012ef9d82c7e5fc464d0227b18bd
Tapas Adhikary <tapas.adhikary@gmail.com>
2023-10-10 08:58:49 +0530

    Error Boundary Implementation

--- react/23-suspense/countries-suspense/src/Countries.jsx
+++ react/23-suspense/countries-suspense/src/Countries.jsx
@@ -1,17 +1,27 @@
 
 import { Suspense } from "react";
+import { ErrorBoundary } from 'react-error-boundary';
+
 import CountryList from "./CountryList";
 import Time from "./Time";
 
 const Countries = () => {
   return (
     <>
-      <Suspense fallback={<p>Loading time...</p>}>
-        <Time />
-      </Suspense>
-      <Suspense fallback={<p>Loading countries...</p>}>
-        <CountryList />
-      </Suspense>
+      <ErrorBoundary
+          fallback={
+            <p className="text-red-600 text-2xl font-bold">
+              Something went wrong...
+            </p>
+          }
+        >
+          <Suspense fallback={<p>Loading time...</p>}>
+            <Time />
+          </Suspense>
+          <Suspense fallback={<p>Loading countries...</p>}>
+            <CountryList />
+          </Suspense>
+      </ErrorBoundary>
     </>
   )
 }
