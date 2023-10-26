eed4d5e6cade838be8ff484d2aec9eb258054532
Tapas Adhikary <tapas.adhikary@gmail.com>
2023-10-26 15:46:39 +0530

    Done with routing-and-layout

--- nextjs-app-router/countries/src/app/page.js
+++ nextjs-app-router/countries/src/app/page.js
@@ -2,8 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Home Page</h1>
-    </main>
   )
 }
