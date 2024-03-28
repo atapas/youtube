0f0a2664f0c7471c029f7eef959c0ca9c8716a6d
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-03-28 10:48:04 +0530

    Adjusted some styles

--- nextjs-app-router/litloop/src/app/store/layout.js
+++ nextjs-app-router/litloop/src/app/store/layout.js
@@ -13,7 +13,7 @@ export default function BookStoreLayout({ children }) {
     <StoreProvider>
       <div className="flex xs:flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row min-h-screen">
         <Sidebar />
-        <div className="flex flex-wrap p-2 justify-center">
+        <div className="flex flex-wrap p-2 justify-center w-full">
           {children}
         </div>  
       </div>
